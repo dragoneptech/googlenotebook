859e435aa891ff7bca907d762be50b297ca8dd55
dragoneptech <62263246+dragoneptech@users.noreply.github.com>
2020-12-28 13:21:22 +0800

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m haizhonglong
 sudo adduser haizhonglong sudo
-echo 'haizhonglong:19840316zss' | sudo chpasswd
+echo 'haizhonglong:123456' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
